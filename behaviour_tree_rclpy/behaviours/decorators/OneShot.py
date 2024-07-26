dc4b1920e4491c895d8ea35a8fa6feacb331eda0
sorawit-inp <sorawit.i@obodroid.com>
2024-07-26 15:30:19 +0700

    change check only_success logic

--- behaviour_tree_rclpy/behaviours/decorators/OneShot.py
+++ behaviour_tree_rclpy/behaviours/decorators/OneShot.py
@@ -12,8 +12,10 @@ class OneShot(pyOneShot):
         name: str = attr['name'] if 'name' in attr.keys() else 'OneShot'
         only_success: typing.Union[str, bool] = attr['only_success']
         if isinstance(only_success, str):
-            if only_success.lower() in ['true', 'false']:
-                only_success = bool(only_success)
+            if only_success.lower() == 'true':
+                only_success = True
+            elif only_success.lower() == 'false':
+                only_success = False
             else:
                 raise ValueError(f"input string {only_success} can't convert to Bool")
         elif not isinstance(only_success, bool):
